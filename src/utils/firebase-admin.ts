31e56ad5810c66783186bb0db22b94e6e60a1521
Thong Phan <phtrithong@gmail.com>
2023-05-26 05:23:49 +0700

    [Fix] firebase admin

--- src/utils/firebase-admin.ts
+++ src/utils/firebase-admin.ts
@@ -1,9 +1,6 @@
 import admin from "firebase-admin";
 
 export const getAdmin: () => admin.app.App = () => {
-  const currentAdmin = admin.app();
-  if (!!currentAdmin) return currentAdmin;
-
   try {
     const app = admin.initializeApp({
       credential: admin.credential.cert({
@@ -13,8 +10,10 @@ export const getAdmin: () => admin.app.App = () => {
           "firebase-adminsdk-m4onf@online-classroom-de70d.iam.gserviceaccount.com",
       }),
     });
+    console.log({ app, key: process.env.FIREBASE_ADMIN_PRIVATE_KEY });
     return app;
   } catch (err) {
+    console.log({ err });
     const currentAdmin = admin.app();
     return currentAdmin;
   }
