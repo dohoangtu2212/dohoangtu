2ba2e7b886ff01a6b1daca001318e8942b5eba55
Thong Phan <phtrithong@gmail.com>
2023-05-26 05:05:34 +0700

    [Fix] Update role api

--- src/pages/api/user/role.ts
+++ src/pages/api/user/role.ts
@@ -13,22 +13,27 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log({ req });
   if (req.method === "PATCH") {
     const admin = getAdmin();
 
     const body = req.body;
     const { phone, email, role } = body as IPatchBody;
+    console.log({ phone, email, role });
+
     if ((!phone && !email) || !role)
       return res.status(400).json({ msg: "Bad request." });
     if (!!phone) {
       const user = await admin.auth().getUserByPhoneNumber(phone);
       if (!user) return res.status(404).json({ msg: "User not found." });
       await admin.auth().setCustomUserClaims(user.uid, { role: role });
+      return res.status(200).end();
     }
     if (!!email) {
       const user = await admin.auth().getUserByEmail(email);
       if (!user) return res.status(404).json({ msg: "User not found." });
       await admin.auth().setCustomUserClaims(user.uid, { role: role });
+      return res.status(200).end();
     }
     return res.status(404).json({ msg: "Bad request." });
   }
