10872a63f273ad084196982c51c5ed8ee88b55e5
Thong Phan <phtrithong@gmail.com>
2023-08-06 02:40:38 +0700

    :lipstick: Update Guide on Home

--- src/components/UI/ArrowDiv/index.tsx
+++ src/components/UI/ArrowDiv/index.tsx
@@ -1,55 +1,67 @@
 import { COLORS } from "@/constants/theme";
-import { Box, BoxProps } from "@chakra-ui/react";
+import { Flex, FlexProps } from "@chakra-ui/react";
 import { FC } from "react";
 
-type ArrowDivProps = BoxProps & {};
-const ArrowDiv: FC<ArrowDivProps> = ({ ...boxProps }) => {
-  const { bgColor = COLORS.starryNightBlue } = boxProps;
+type ArrowDivProps = FlexProps & {};
+const ArrowDiv: FC<ArrowDivProps> = ({ children, ...flexProps }) => {
+  const { bgColor = COLORS.starryNightBlue } = flexProps;
 
   return (
-    <Box
+    <Flex
       bgColor={bgColor}
-      h={{ base: "3rem", md: "6rem" }}
-      w="fit-content"
+      h={{ base: "3rem", md: "6rem", lg: "10rem" }}
       aspectRatio="5/4"
       position="relative"
+      alignItems="center"
+      justifyContent="center"
       _after={{
         color: bgColor,
-        borderLeft: { base: "1.5rem solid", md: "3rem solid" },
+        borderLeft: {
+          base: "1.5rem solid",
+          md: "3rem solid",
+          lg: "5rem solid",
+        },
         borderTop: {
           base: "1.5rem solid transparent",
           md: "3rem solid transparent",
+          lg: "5rem solid transparent",
         },
         borderBottom: {
           base: "1.5rem solid transparent",
           md: "3rem solid transparent",
+          lg: "5rem solid transparent",
         },
         display: "inline-block",
         content: '""',
         position: "absolute",
-        right: { base: "-1.5rem", md: "-3rem" },
+        right: { base: "-1.5rem", md: "-3rem", lg: "-5rem" },
       }}
       _before={{
         color: COLORS.white,
         borderLeft: {
           base: `1.5rem solid ${COLORS.white}`,
           md: `3rem solid ${COLORS.white}`,
+          lg: `5rem solid ${COLORS.white}`,
         },
         borderTop: {
           base: `1.5rem solid transparent`,
           md: "3rem solid transparent",
+          lg: `5rem solid transparent`,
         },
         borderBottom: {
           base: "1.5rem solid transparent",
           md: "3rem solid transparent",
+          lg: "5rem solid transparent",
         },
         display: "inline-block",
         content: '""',
         position: "absolute",
         left: "-1px",
       }}
-      {...boxProps}
-    ></Box>
+      {...flexProps}
+    >
+      {children}
+    </Flex>
   );
 };
 
--- src/views/Home/Main/Guide/index.tsx
+++ src/views/Home/Main/Guide/index.tsx
@@ -11,81 +11,98 @@ const Guide = () => {
       <Text fontSize={{ base: "1.75rem", md: "2.5rem" }} fontWeight="700">
         Quy trình đăng kí học
       </Text>
-      <Flex py="2rem" alignItems="center" justifyContent="center">
-        <Container zIndex="40">
-          <DescriptionContainer>
-            <Text w="max-content" fontSize={{ base: "1rem", md: "1.5rem" }}>
-              đăng kí tài khoản
-            </Text>
-            <Divider
-              orientation="vertical"
-              borderColor={COLORS.starryNightBlue}
-            />
-          </DescriptionContainer>
-          <ArrowDiv bgColor={COLORS.whiteSatin} position="relative">
-            <DisplayImage imageSrc="/images/dang-ki-tai-khoan.png" />
-          </ArrowDiv>
-          <Box flex="1" />
-        </Container>
-        <Container zIndex="30">
-          <Box flex="1" />
-          <ArrowDiv bgColor={COLORS.windmillWings} position="relative">
-            <DisplayImage imageSrc="/images/chon-khoa-hoc.png" />
-          </ArrowDiv>
-          <DescriptionContainer>
-            <Divider
-              orientation="vertical"
-              borderColor={COLORS.starryNightBlue}
-            />
-            <Text
-              textAlign="center"
-              w="max-content"
-              fontSize={{ base: "1rem", md: "1.5rem" }}
-            >
-              chọn khoá học
-            </Text>
-          </DescriptionContainer>
-        </Container>
-        <Container zIndex="20">
-          <DescriptionContainer>
-            <Text
-              textAlign="center"
-              w="max-content"
-              fontSize={{ base: "1rem", md: "1.5rem" }}
-            >
-              nộp học phí
-            </Text>
-            <Divider
-              orientation="vertical"
-              borderColor={COLORS.starryNightBlue}
-            />
-          </DescriptionContainer>
-          <ArrowDiv bgColor={COLORS.summerBlue} position="relative">
-            <DisplayImage imageSrc="/images/nop-hoc-phi.png" />
-          </ArrowDiv>
-          <Box flex="1" />
-        </Container>
-        <Container zIndex="10">
-          <Box flex="1" />
-          <ArrowDiv bgColor={COLORS.blueLapis} position="relative">
-            <DisplayImage imageSrc="/images/bat-dau-hoc.png" />
-          </ArrowDiv>
-          <DescriptionContainer>
-            <Divider
-              orientation="vertical"
-              borderColor={COLORS.starryNightBlue}
-            />
-            <Text
-              textAlign="center"
-              w="max-content"
-              fontSize={{ base: "1rem", md: "1.5rem" }}
-            >
-              bắt đầu học
-            </Text>
-          </DescriptionContainer>
-        </Container>
+      <Flex
+        py="2rem"
+        alignItems="center"
+        justifyContent="center"
+        gap={{ base: "0.5rem", md: "1rem" }}
+      >
+        <RegisteStep />
+        <CourseStep />
+        <FeeStep />
+        <LearnStep />
       </Flex>
     </Flex>
+  );
+};
+
+const RegisteStep = () => {
+  return (
+    <Container zIndex="40">
+      <DescriptionContainer>
+        <Text w="max-content" fontSize={{ base: "1rem", md: "1.5rem" }}>
+          đăng kí tài khoản
+        </Text>
+        <Pin direction="up" />
+      </DescriptionContainer>
+      <ArrowDiv bgColor={COLORS.whiteSatin} position="relative">
+        <DisplayImage imageSrc="/images/dang-ki-tai-khoan.png" />
+      </ArrowDiv>
+      <Box flex="1" />
+    </Container>
+  );
+};
+
+const CourseStep = () => {
+  return (
+    <Container zIndex="30">
+      <Box flex="1" />
+      <ArrowDiv bgColor={COLORS.windmillWings} position="relative">
+        <DisplayImage imageSrc="/images/chon-khoa-hoc.png" />
+      </ArrowDiv>
+      <DescriptionContainer>
+        <Pin direction="down" />
+        <Text
+          textAlign="center"
+          w="max-content"
+          fontSize={{ base: "1rem", md: "1.5rem" }}
+        >
+          chọn khoá học
+        </Text>
+      </DescriptionContainer>
+    </Container>
+  );
+};
+
+const FeeStep = () => {
+  return (
+    <Container zIndex="20">
+      <DescriptionContainer>
+        <Text
+          textAlign="center"
+          w="max-content"
+          fontSize={{ base: "1rem", md: "1.5rem" }}
+        >
+          nộp học phí
+        </Text>
+        <Pin direction="up" />
+      </DescriptionContainer>
+      <ArrowDiv bgColor={COLORS.summerBlue} position="relative">
+        <DisplayImage imageSrc="/images/nop-hoc-phi.png" />
+      </ArrowDiv>
+      <Box flex="1" />
+    </Container>
+  );
+};
+
+const LearnStep = () => {
+  return (
+    <Container zIndex="10">
+      <Box flex="1" />
+      <ArrowDiv bgColor={COLORS.blueLapis} position="relative">
+        <DisplayImage imageSrc="/images/bat-dau-hoc.png" />
+      </ArrowDiv>
+      <DescriptionContainer>
+        <Pin direction="down" />
+        <Text
+          textAlign="center"
+          w="max-content"
+          fontSize={{ base: "1rem", md: "1.5rem" }}
+        >
+          bắt đầu học
+        </Text>
+      </DescriptionContainer>
+    </Container>
   );
 };
 
@@ -94,8 +111,8 @@ const Container: FC<ContainerProps> = ({ children, ...flexProps }) => {
   return (
     <Flex
       flexDir="column"
-      h="15rem"
-      w={{ base: "4rem", md: "8rem" }}
+      h={{ base: "15rem", md: "20rem", lg: "30rem" }}
+      w={{ base: "4rem", md: "8rem", lg: "16rem" }}
       {...flexProps}
     >
       {children}
@@ -127,11 +144,11 @@ type DisplayImageProps = {
 const DisplayImage: FC<DisplayImageProps> = ({ imageSrc }) => {
   return (
     <Box
-      w={{ base: "2.5rem", md: "4.5rem" }}
-      h={{ base: "2.25rem", md: "4.5rem" }}
+      w={{ base: "2.5rem", md: "4.5rem", lg: "6rem" }}
+      h={{ base: "2.25rem", md: "4.5rem", lg: "6rem" }}
       position="absolute"
-      left={{ base: "1.5rem", md: "3.25rem" }}
-      top={{ base: "0.5rem", md: "0.75rem" }}
+      left={{ base: "1.5rem", md: "3.25rem", lg: "8rem" }}
+      top={{ base: "0.5rem", md: "0.75rem", lg: '2rem' }}
       zIndex="10"
     >
       <Image
@@ -143,6 +160,31 @@ const DisplayImage: FC<DisplayImageProps> = ({ imageSrc }) => {
         }}
       />
     </Box>
+  );
+};
+
+type PinProps = {
+  direction?: "up" | "down";
+};
+const Pin: FC<PinProps> = ({ direction = "up" }) => {
+  return (
+    <Flex
+      h="100%"
+      flexDir={direction === "up" ? "column" : "column-reverse"}
+      alignItems="center"
+    >
+      <Box
+        w="0.4rem"
+        h="0.4rem"
+        borderRadius="50%"
+        bgColor={COLORS.starryNightBlue}
+      />
+      <Divider
+        orientation="vertical"
+        borderColor={COLORS.starryNightBlue}
+        opacity="1"
+      />
+    </Flex>
   );
 };
 
