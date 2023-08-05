84bd34f9d9f84b6e43fb6dcefef20976e49c751d
Thong Phan <phtrithong@gmail.com>
2023-08-06 02:49:33 +0700

    :sparkles: Add og tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import Head from "next/head";
 import Home from "@/views/Home";
-import PageContainer from "@/components/Layout/PageContainer";
 
 const HomePage = () => {
   return (
     <>
       <Head>
         <title>Trang chủ</title>
+        <meta name="description" content="Đỗ Hoàng Tú" key="desc" />
+        <meta property="og:title" content="Đỗ Hoàng Tú" />
+        <meta
+          property="og:description"
+          content="Chinh phục môn TOÁN kì thi THPT QG"
+        />
+        <meta
+          property="og:image"
+          content="https://www.dohoangtu.edu.vn/images/thumbnail.png"
+        />
       </Head>
       <Home />
     </>
