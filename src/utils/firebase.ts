bdc1ca0d7397f5ff570c03ac97682038c17b7084
sanglee <lyphuocsang96@gmail.com>
2024-06-13 16:12:48 +0700

    cmt

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -17,14 +17,14 @@ import type { FirebaseOptions } from "firebase/app";
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_CONFIG_API_KEY,
-  authDomain: "dohoangtu-5ae0c.firebaseapp.com",
+  authDomain: "online-classroom-de70d.firebaseapp.com",
   projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: "dohoangtu-5ae0c.appspot.com",
-  messagingSenderId: "697997266316",
-  appId: "1:697997266316:web:0f5078a4e1a10d6a061aa9",
-  measurementId: "",
+  storageBucket: "online-classroom-de70d.appspot.com",
+  messagingSenderId: "862093748361",
+  appId: "1:862093748361:web:cfc64b84080d8861b07e58",
+  measurementId: "G-LMXVLSZ36X",
   databaseURL:
-    "https://dohoangtu-5ae0c-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    "https://online-classroom-de70d-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
 
 const app = initializeApp(firebaseConfig);
